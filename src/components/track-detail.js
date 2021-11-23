34ba3e76015316cde4f65f91b764834598e202f2
Austen Mack <amack@butterpayments.com>
2021-11-23 14:47:44 -0700

    refactor fe from length to durationInSeconds

--- src/components/track-detail.js
+++ src/components/track-detail.js
@@ -24,7 +24,8 @@ const TrackDetail = ({ track }) => {
     description,
     thumbnail,
     author,
-    length,
+    // length,
+    durationInSeconds,
     modulesCount,
     modules,
     numberOfViews,
@@ -50,7 +51,7 @@ const TrackDetail = ({ track }) => {
             </IconAndLabel>
             <IconAndLabel>
               <IconTime width="14px" />
-              <div>{humanReadableTimeFromSeconds(length)}</div>
+              <div>{humanReadableTimeFromSeconds(durationInSeconds)}</div>
             </IconAndLabel>
           </DetailItem>
           <DetailItem>
@@ -78,7 +79,7 @@ const TrackDetail = ({ track }) => {
                 <li key={module.title}>
                   <div>{module.title}</div>
                   <ModuleLength>
-                    {humanReadableTimeFromSeconds(module.length)}
+                    {humanReadableTimeFromSeconds(module.durationInSeconds)}
                   </ModuleLength>
                 </li>
               ))}
