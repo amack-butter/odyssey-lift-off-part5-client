1c8c0c4fd9c760651f7c8cd2f568e7c660df7b57
Austen Mack <amack@butterpayments.com>
2021-11-30 11:51:43 -0700

    move uri to .env

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://server-catstronauts-3.herokuapp.com/',
+  uri: process.env.REACT_APP_APOLLO_SERVER_URI,
   cache: new InMemoryCache(),
 });
 
